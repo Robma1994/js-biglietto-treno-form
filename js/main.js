2035593fbb687dcac56c808681b1bfda6f2d7ba5
Roberto <robertomartinonwg@gmail.com>
2021-07-24 12:12:15 +0200

    La funzione Genera è stata completata.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var btnClick = document.getElementById("genera");
 btnClick.addEventListener("click",
     function() {
     var nome = document.getElementById("nome").value;
-    console.log(nome);
+    document.getElementById("nomeInserito").innerHTML = nome;
     var km = document.getElementById("numero").value;
     var fasciaEta = document.getElementById("fasciaEta").value;
     var prezzoKm = 0.21;
@@ -16,25 +16,41 @@ btnClick.addEventListener("click",
     var prezzoMinorenne = (prezzoLordo - scontoMinorenne);
     var prezzoOver65 = (prezzoLordo - scontoOver65);
     if (fasciaEta == "minorenne") {
-        console.log(prezzoMinorenne)
+        document.getElementById("prezzoMinorenne").innerHTML = ("Il prezzo del tuo biglietto è di " + prezzoMinorenne + "  €");
+        document.getElementById("scontoMinorenne").innerHTML = ("Sconto applicato: Minorenne");
     } else if ( fasciaEta == "over65") {
-        console.log(prezzoOver65);
+        document.getElementById("prezzoOver65").innerHTML = ("Il prezzo del tuo biglietto è di " + prezzoOver65 + "  €");
+        document.getElementById("scontoOver65").innerHTML = ("Sconto applicato: Over 65");
     } 
     else {
-        console.log(prezzoLordo);
+        document.getElementById("prezzoMaggiorenne").innerHTML = ("Il prezzo del tuo biglietto è di " + prezzoLordo + "  €");
+        document.getElementById("noScontoMaggiorenne").innerHTML = ("Nessuno sconto applicato");
     }
+    var codiceCP = rndnumero(10000, 9000);
+    document.getElementById("codiceCP").innerHTML = codiceCP;
+    var numeroCarrozza = rndnumero(15,1);
+    document.getElementById("numeroCarrozza").innerHTML = numeroCarrozza;
+    
+    var bigliettoVisibile = document.getElementById("bigliettoFantasma");
+    bigliettoVisibile.style.display = "block";
 }
 );
+    
+/*FUNZIONI*/
+function rndnumero(max, min) {
+    var numeroRandom = Math.floor(Math.random() * (max - min) + min);
+    return numeroRandom;
+}
 
-
+/*FINE FUNZIONI*/
 
 //BIGLIETTO
 
 //Creiamo un finto biglietto del treno con:
-// 1 - Nome passeggero
+// 1 - Nome passeggero (ok)
 // 2 - Codice treno (numero casuale tra 90000 e 100000 escluso)
 // 3 -Numero carrozza
-// 4 - Prezzo calcolato
-// 5 - Categoria selezionata dall'utente
+// 4 - Prezzo calcolato (ok)
+// 5 - Categoria selezionata dall'utente (ok)
 /* 6 - Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", 
 se clicchiamo su annulla dobbiamo ripulire il form.*/
